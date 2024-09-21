dca5d109d62261fa06f36ee9850867dce7a44d62
menna allah <menatallah260@gmail.com>
2024-09-21 16:24:37 +0300

    updating-onboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:tasking/presentation/screens/home_screen.dart';
 import 'package:tasking/presentation/screens/onboarding_screen.dart';
 
 void main() {
--- lib/presentation/screens/onboarding_screen.dart
+++ lib/presentation/screens/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -15,23 +16,33 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       body: PageView(
         controller: nextPage,
         children: [
-
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 100,),
-              Image.asset('assets/images/first.jpeg',fit: BoxFit.fill,),
-              SizedBox(height: 30,),
-              Text(
+              const SizedBox(
+                height: 100,
+              ),
+              Image.asset(
+                'assets/images/first.jpeg',
+                fit: BoxFit.fill,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
                 'Hello in E commerse',
                 style: TextStyle(
                   fontSize: 24,
                 ),
               ),
-              SizedBox(height: 30,),
+              const SizedBox(
+                height: 30,
+              ),
               InkWell(
-                onTap: (){
-                  nextPage.animateToPage(1, duration: Duration(milliseconds: 700), curve: Curves.easeIn);
+                onTap: () {
+                  nextPage.animateToPage(1,
+                      duration: const Duration(milliseconds: 700),
+                      curve: Curves.easeIn);
                 },
                 child: Container(
                   width: 100,
@@ -40,9 +51,123 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     borderRadius: BorderRadius.circular(20),
                     color: Colors.black,
                   ),
-                  child: Center(
+                  child: const Center(
+                    child: Text(
+                      'Next',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SmoothPageIndicator(
+                controller: nextPage,
+                count: 3,
+                effect: const WormEffect(
+                  activeDotColor: Colors.black,
+                  dotColor: Colors.grey,
+                  dotHeight: 10,
+                  dotWidth: 10,
+                  spacing: 16.0,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Image.asset('assets/images/second.jpeg'),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'Continu to the third Screen',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              InkWell(
+                onTap: () {
+                  nextPage.animateToPage(2,
+                      duration: const Duration(milliseconds: 700),
+                      curve: Curves.easeIn);
+                },
+                child: Container(
+                  width: 100,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.black,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Next',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SmoothPageIndicator(
+                controller: nextPage,
+                count: 3,
+                effect: const WormEffect(
+                  activeDotColor: Colors.black,
+                  dotColor: Colors.grey,
+                  dotHeight: 10,
+                  dotWidth: 10,
+                  spacing: 16.0,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              Image.asset('assets/images/third.jpeg'),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'Continu to the Home Screen',
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  width: 100,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.black,
+                  ),
+                  child: const Center(
                       child: Text(
-                    'Next',
+                    'Get Started',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 20,
@@ -50,66 +175,18 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   )),
                 ),
               ),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 100,),
-              Image.asset('assets/images/second.jpeg'),
-              SizedBox(height: 30,),
-              Text(
-                'Continu to the third Screen',
-                style: TextStyle(
-                  fontSize: 24,
-                ),
+              const SizedBox(
+                height: 30,
               ),
-              SizedBox(height: 30,),
-              InkWell(
-                onTap: (){
-                  nextPage.animateToPage(2, duration: Duration(milliseconds: 700), curve: Curves.easeIn);
-                },
-                child: Container(
-                  width: 100,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.black,
-                  ),
-                  child: Center(child: Text('Next',style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),)),
-                ),
-              ),
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 100,),
-              Image.asset('assets/images/third.jpeg'),
-              SizedBox(height: 30,),
-              Text(
-                'Continu to the Home Screen',
-                style: TextStyle(
-                  fontSize: 24,
-                ),
-              ),
-              SizedBox(height: 30,),
-              InkWell(
-                onTap: (){},
-                child: Container(
-                  width: 100,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.black,
-                  ),
-                  child: Center(child: Text('Next',style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),)),
+              SmoothPageIndicator(
+                controller: nextPage,
+                count: 3,
+                effect: const WormEffect(
+                  activeDotColor: Colors.black,
+                  dotColor: Colors.grey,
+                  dotHeight: 10,
+                  dotWidth: 10,
+                  spacing: 16.0,
                 ),
               ),
             ],
