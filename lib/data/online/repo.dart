8a3d222a74b4887f58a1ee1e548199a2348f8f16
abdulazizamori <abdulaziz.amori@icloud.com>
2024-09-21 15:11:48 +0300

    add data online

---
+++ lib/data/online/repo.dart
@@ -0,0 +1,30 @@
+import 'package:tasking/data/online/dio.dart';
+import 'package:dio/dio.dart';
+import 'package:tasking/data/online/endpoints.dart';
+
+abstract class Repotisory{
+
+  final DioHelper? dioHelper;
+
+  Repotisory(this.dioHelper);
+
+  Future<Response?> getAllData();
+
+}
+
+class RepoImplementation extends Repotisory{
+  RepoImplementation(super.dioHelper);
+
+  @override
+  Future<Response?> getAllData() async{
+    return await dioHelper?.getData(url: baseUrl+endpoint);
+  }
+
+
+
+
+
+
+
+
+}
