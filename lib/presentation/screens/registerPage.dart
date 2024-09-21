8627ac8c7a22a6d74b15031a84be2bba3e6823fb
wwalid1 <w.walid@nu.edu.eg>
2024-09-21 22:12:04 +0300

    sign in page

--- lib/presentation/screens/registerPage.dart
+++ lib/presentation/screens/registerPage.dart
@@ -12,28 +12,101 @@ class _RegisterpageState extends State<Registerpage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Register"),
+          title: Text("Sign up"),
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 20,top: 20),
-              child: Container(
-                width: 600,
-                child: TextFormField(
-                    decoration: InputDecoration(
-
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0)),
-                      hintText: "Enter your Name",
-                      //prefixIcon: Icon(Icons),
-
-                    )),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                "Create your account",
+                style: TextStyle(fontSize: 15, color: Colors.grey[700]),
               ),
-            )
-
-          ],
+              SizedBox(height: 40,),
+              InkWell(
+                  child: CircleAvatar(
+                backgroundColor: Colors.black,
+                radius: 40.0,
+                child: CircleAvatar(
+                  radius: 38.0,
+                  child: ClipOval(
+                    child: Image.asset('images/newimage.png'),
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+              )),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+                child: Container(
+                  width: 600,
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          hintText: "Enter Your Name",
+                          prefixIcon: const Icon(Icons.person))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+                child: Container(
+                  width: 600,
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          hintText: "Enter Your Email",
+                          prefixIcon: const Icon(Icons.email))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+                child: Container(
+                  width: 500,
+                  child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          hintText: "Enter Your Password",
+                          prefixIcon: const Icon(Icons.password))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
+                child: Container(
+                  width: 500,
+                  child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0)),
+                          hintText: "Confirm Password",
+                          prefixIcon: const Icon(Icons.password))),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Sign up")),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Already have an account?"),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(color: Colors.purple),
+                      ))
+                ],
+              )
+            ],
+          ),
         ));
   }
 }
